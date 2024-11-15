e06978761d9fccc9967b3fca83162c603fd0a20f
bodhispace <bodhi.space24@gmail.com>
2024-11-15 16:44:55 +0530

    About us Page updated

--- src/app/industries/page.tsx
+++ src/app/industries/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import IndustriesSection from "@/components/Industries/IndustriesSection";
+import Breadcrumb from "@/components/Common/Breadcrumb"; // Import the Breadcrumb component
 
 import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Our Services | Comprehensive Odoo ERP Solutions",
-  description: "Explore our wide range of Odoo ERP services, including implementation, customization, migration, support, and training. Empower your business with Inerpulse LLP.",
+  description:
+    "Explore our wide range of Odoo ERP services, including implementation, customization, migration, support, and training. Empower your business with Inerpulse LLP.",
 };
-
 
 const ContactPage = () => {
   return (
@@ -17,10 +18,9 @@ const ContactPage = () => {
         pageName="Our Services"
         description="At Inerpulse LLP, we specialize in delivering comprehensive Odoo ERP solutions tailored to your business needs. From initial implementation to ongoing support and upgrades, our team ensures your ERP system operates seamlessly, empowering your business to thrive."
       />
-
       <IndustriesSection />
     </>
   );
 };
 
-export default IndustriesSection;
+export default ContactPage;
