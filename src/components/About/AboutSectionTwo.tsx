3190e88403cf804480f938d25b530050206ef2ca
bodhispace <bodhi.space24@gmail.com>
2024-11-24 22:42:59 +0530

    images updated

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -6,25 +6,6 @@ const AboutSectionTwo = () => {
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
-            <div
-              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
-              <Image
-                src="/images/about/garments.png"
-                alt="about image"
-                fill
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
-              />
-              <Image
-                src="/images/about/garments.png"
-                alt="about image"
-                fill
-                className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
-              />
-            </div>
-          </div>
-          <div className="w-full px-4 lg:w-1/2">
             <div className="max-w-[470px]">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
@@ -50,6 +31,25 @@ const AboutSectionTwo = () => {
                   Ensure compliance, manage inventory, and enhance the supply chain in the Pharmaceutical sector with Odoo's robust solutions.
                 </p>
               </div>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-1/2">
+            <div
+              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              data-wow-delay=".15s"
+            >
+              <Image
+                src="/images/about/garments.png"
+                alt="about image"
+                fill
+                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
+              />
+              <Image
+                src="/images/about/garments.png"
+                alt="about image"
+                fill
+                className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
+              />
             </div>
           </div>
         </div>
